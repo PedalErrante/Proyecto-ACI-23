8f196c050adfe0e896dfeeb901f785482d61071f
Mike <m.navarro2108@gmail.com>
2023-12-20 00:19:08 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -4,5 +4,22 @@ import requests
 import json
 import conf
 
-print(conf.usuario)
-print(conf.clave)
+requests.packages.urllib3.disable_warnings()
+
+url = "https://10.10.20.14/api/aaaLogin.json"
+data = {
+    "aaaUser" : {
+        "attributes" : {
+            "name" : conf.usuario,
+            "pwd" : conf.clave
+        }
+    }
+}
+
+cabecera = {"Content-Type": "application/json"}
+repuesta = requests.post(url, json.dumps(data), headers = cabecera, verify= False)
+respuesta_json = repuesta.json()
+print(respuesta_json)
+API_Token = respuesta_json["imdata"][0]["aaaLogin"]["attributes"]["token"]
+print("API-Token: "+ API_Token)
+
